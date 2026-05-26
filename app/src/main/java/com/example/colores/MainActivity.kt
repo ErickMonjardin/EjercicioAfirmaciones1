07df678c7929a35e74d837e88fe213bbf92b3c27
ErickMonjardin <erick.monjardin@uabc.edu.mx>
2026-05-25 22:19:50 -0700

    Initial commit

---
+++ app/src/main/java/com/example/colores/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.colores
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.colores.data.Datasource
+import com.example.colores.model.Affirmation
+import com.example.colores.ui.theme.ColoresTheme
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ColoresTheme {
+                AffirmationsApp()
+            }
+        }
+    }
+}
+@Composable
+fun AffirmationsApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmations()
+        )
+    }
+}
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmation>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+@Composable
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun AffirmationPreview() {
+    ColoresTheme {
+        AffirmationCard(
+            affirmation = Affirmation(
+                R.string.affirmation1,
+                R.drawable.image1
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/colores/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.example.colores.model
+
+data class Affirmation(
+    val stringResourceId: Int,
+    val imageResourceId: Int
+)
